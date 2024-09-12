4f143e2ec937456493c14a8fe292d87ddd2b5242
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-09-12 18:55:29 -0500

    first commit of all files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from 'vue'
+
+
+</script>
+
+
+<template>
+  <main class="content">
+    <h1 class="main-title"> 
+      Contact List
+    </h1>
+    <h2>Input your contact information.</h2> 
+    <h3>First name is needed in order to create list</h3>
+    <section class="formItems">
+      
+
+
+    </section>
+
+  </main>
+
+
+</template>
+
+
+<style scoped>
+
+
+</style>
