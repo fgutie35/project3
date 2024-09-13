385e28e4d4913bedb749d279e046e920bc53b0f9
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-09-12 19:14:07 -0500

    Converted the input name and number into the vue code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script setup>
 import {ref} from 'vue'
 
+const myArray = ref([])
+const name = ref('')
+const input_name = ref('')
+const input_number = ref('')
+
+const toAdd = () =>{
+  if(intput_content.value.trim() === '' || input_number.value.trim() === ''){
+    return 
+ }
+
+ MyArray.value.push({
+  content: input_name.value,
+  number: input_number,
+  done: false
+ })
+
+ input_name.value = ''
+ input_number.value = ''
+
+}
 
 </script>
 
@@ -12,11 +32,26 @@ import {ref} from 'vue'
     </h1>
     <h2>Input your contact information.</h2> 
     <h3>First name is needed in order to create list</h3>
-    <section class="formItems">
-      
+
+  <section class="formItems">
+    <form @submit.prevent = "toAdd">
+        First name: <br />
+      <input type="text" placeholder="EnterFirstName" v-model="input_name">
+      <br /><br />
+
+      Phone Number: <br />
+      <input type="text" placeholder="EnterFirstName" v-model="input_number">
+      <br /><br />
 
 
-    </section>
+
+    </form>
+
+  </section>
+
+
+
+
 
   </main>
 
